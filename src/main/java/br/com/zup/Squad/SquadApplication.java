bb2fc44fca1862acbc77b2802c11696f209a4777
jordihofc <jordi.silva@ufv.br>
2021-03-29 14:41:08 -0300

    criando o projeto

---
+++ src/main/java/br/com/zup/Squad/SquadApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.Squad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SquadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/Squad/SquadApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.Squad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
